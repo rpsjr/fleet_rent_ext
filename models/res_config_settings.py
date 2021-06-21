44e26794a96eba93f869f525754333980ef0b0ef
RPSJR <raimundops.jr@gmail.com>
2021-06-21 16:19:34 -0300

    [COR]

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
-# See LICENSE file for full copyright and licensing details.
+#See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = ["res.config.settings"]
+    _inherit = ['res.config.settings']
 
-    deposit_product_id = fields.Many2one(
-        "product.product",
-        string="Deposit product",
-        help="Standard deposit product usend in accounting",
-        config_parameter="fleet_rent.deposit_product_id",
-    )
-    fleet_rent_fiscal_postion_id = fields.Many2one(
-        "account.fiscal.position",
-        string="Fiscal Position",
-        help="Standard fiscal position to rent contract",
-        config_parameter="fleet_rent.fiscal_postion_id",
-    )
+    deposit_product = fields.Many2one('product.product',
+                                  string='Deposit product',
+                                  help="Standard deposit product usend in accounting",
+                                  config_parameter='fleet_rent.deposit_product')
